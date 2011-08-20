6ae70eea369b5324fdd9328a4b20b1156b48e6be
Juha Autero <jautero@iki.fi>
2011-08-20 20:41:18 +0300

    Added imagedir and changed mplayer to use individual images.

--- screensaver.sh
+++ screensaver.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
-mencoder "mf://image*.jpg" -o workmovie.mpg -ovc x264
-mv workmovie.mpg movie.mpg
-mplayer -wid $XSCREENSAVER_WINDOW -loop 0 -fs movie.mpg
+IMAGEDIR=/var/lib/netf/
+mplayer -wid $XSCREENSAVER_WINDOW -loop 0 -fs -mf on:type=png "${IMAGEDIR}/image*.png"
 
 
