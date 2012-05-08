23073cfbf9c329219292ade637b6380d80ef9d4d
Hacklab-olohuonelappari <Hacklab-olohuonelappari>
2012-05-09 00:12:58 +0300

    removed looping

--- movie.sh
+++ movie.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-old_count=0
+mencoder "mf://image*.jpg" -o workmovie.mpg -ovc x264
 
-while true; do
-    img_count=`ls image*.jpg|wc -l`
-    delta=`expr $img_count - $old_count`
-    echo $delta
-    if [ $delta -gt 24 ]; then 
-	mencoder "mf://image*.jpg" -o workmovie.mpg -ovc x264
-	kill $!
-	mv workmovie.mpg movie.mpg
-	mplayer -loop 0 -fs movie.mpg &
-	old_count=$img_count
-    fi
-    sleep 60
-done
